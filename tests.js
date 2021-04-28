e08e2b4e8b3eb1dc46c88bd0924678135d5f710b
ALeksey <alexsey.prohin@yandex.ru>
2021-04-28 14:29:34 +0300

    polufinal

--- tests.js
+++ tests.js
@@ -33,10 +33,9 @@ testCase('/GET posts', function () {
     });
 
 
-});
 //tests for GET	/posts/:id - please create several tests (7 tests or more)
 // test GET /posts/:id
-testCase('/GET posts/:id', function () {
+//testCase('/GET posts/:id', function () {
     it('it should GET a posts by id', (done) => {
         const postsId = 1;
         chai.request('https://jsonplaceholder.typicode.com')
@@ -53,30 +52,30 @@ testCase('/GET posts/:id', function () {
                 done();
             });
     });
-});
 
 
-it('it should GET return array', (done) => {
-    const userId = '?userid=1';
-    chai.request('https://jsonplaceholder.typicode.com')
-        .get('/posts/' + userId)
-        .end((err, res) => {
-            res.should.have.status(200);
-            res.body.should.be.a('array');
-            done();
-        });
-});
+    it('it should GET return array', (done) => {
+        const userId = '?userid=1';
+        chai.request('https://jsonplaceholder.typicode.com')
+            .get('/posts/' + userId)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('array');
+                done();
+            });
+    });
 
 
-it('it should NOT GET one the posts', (done) => {
-    const postsId = 123;
-    chai.request('https://jsonplaceholder.typicode.com')
-        .get('/posts/' + postsId)
-        .end((err, res) => {
-            res.should.have.status(404);
-            res.text.should.be.eq("{}");
-            done();
-        });
+    it('it should NOT GET one the posts', (done) => {
+        const postsId = 123;
+        chai.request('https://jsonplaceholder.typicode.com')
+            .get('/posts/' + postsId)
+            .end((err, res) => {
+                res.should.have.status(404);
+                res.text.should.be.eq("{}");
+                done();
+            });
+    });
 });
 // ТестКейс проходит при условии, статус страницы 404
 /*
@@ -174,6 +173,28 @@ testCase('/PUT posts/:id', function () {
                 res.body.should.have.property('userId').eq(1);
                 res.body.should.have.property('id').eq(1);
                 res.body.should.have.property('title').eq("sunt aut facere repellat provident occaecati excepturi optio reprehenderit");
+                res.body.should.have.property('body').eq("quia et suscipit\\nsuscipit recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum est autem sunt rem eveniet architecto");
+                done();
+            });
+    });
+
+    it('it should change title', (done) => {
+        const putUrl = 1;
+        const posts = {
+            "userId": 1,
+            "id": 1,
+            "title": "sunt",
+            "body": "quia et suscipit\\nsuscipit recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum est autem sunt rem eveniet architecto"
+        };
+        chai.request('https://jsonplaceholder.typicode.com')
+            .put('/posts/' + putUrl)
+            .send(posts)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                res.body.should.have.property('userId').eq(1);
+                res.body.should.have.property('id').eq(1);
+                res.body.should.have.property('title').eq("sunt");// aut facere repellat provident occaecati excepturi optio reprehenderit");
                 res.body.should.have.property('body').eq("quia et suscipit\\nsuscipit recusandae consequuntur expedita et cum\\nreprehenderit molestiae ut ut quas totam\\nnostrum rerum est autem sunt rem eveniet architecto");
                 done();
             });
